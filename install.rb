a2771e4953dc192d523aee64908712acfb4ce743
Marcos Tapajos <tapajos@gmail.com>
2008-03-11 19:14:36 +0000

    Trabalhando na instalacao

--- install.rb
+++ install.rb
@@ -1,4 +1,6 @@
 sample = File.expand_path(File.dirname(__FILE__) + "/samples/skip_selenium_and_coverage.rake")
 integration = File.expand_path(RAILS_ROOT + "/lib/tasks/integration.rake")
 
-FileUtils.cp sample, integration
+p sample
+p integration
+#FileUtils.cp sample, integration
