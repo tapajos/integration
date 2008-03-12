1e060b273e2bc43288058f6ec050ef6e52782b5a
Marcos Tapajos <tapajos@gmail.com>
2008-03-12 18:25:59 +0000

    Trocando de page para docs e ajustando o README.

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
+require 'fileutils'
 sample = File.expand_path(File.dirname(__FILE__) + "/samples/skip_selenium_and_coverage.rake")
 integration = File.expand_path(RAILS_ROOT + "/lib/tasks/integration.rake")
 
-require 'fileutils'
 FileUtils.cp sample, integration
 
 puts "Integration rake file created."
