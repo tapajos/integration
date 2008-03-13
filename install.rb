8afad75c97a77cb168a9e9e9767551464904cbaa
Marcos Tapajos <tapajos@gmail.com>
2008-03-13 03:05:09 +0000

    Merge commit 'improveit/master'

--- install.rb
+++ install.rb
@@ -3,6 +3,6 @@ sample = File.expand_path(File.dirname(__FILE__) + "/samples/skip_selenium_and_c
 integration = File.expand_path(RAILS_ROOT + "/lib/tasks/integration.rake")
 
 FileUtils.cp sample, integration
+puts `svn add #{integration}`
+puts "Integration rake file (#{integration}) created and added to subversion. If you don't intend to use this file, run svn remove ..."
 
-puts "Integration rake file created."
-puts integration
