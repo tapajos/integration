a18f51d19f4972664cf83bd7d5970a8a44e9d553
Tapajós <tapajos@gmail.com>
2008-09-08 22:09:59 -0300

    Remove svn add. It is wrong when you are using git instead of svn.

--- install.rb
+++ install.rb
@@ -3,6 +3,3 @@ sample = File.expand_path(File.dirname(__FILE__) + "/samples/skip_selenium_and_c
 integration = File.expand_path(RAILS_ROOT + "/lib/tasks/integration.rake")
 
 FileUtils.cp sample, integration
-puts `svn add #{integration}`
-puts "Integration rake file (#{integration}) created and added to subversion. If you don't intend to use this file, run:"
-puts "svn remove #{integration}"
