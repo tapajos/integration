a7596e541449518263fb596d8d638d654ad1636c
Sylvestre Mergulhão <contato@mergulhao.info>
2011-06-01 13:01:48 -0300

    conditionally load tasks if its a Rake environment

--- lib/integration.rb
+++ lib/integration.rb
@@ -1,1 +1,1 @@
-Dir["#{Gem.searcher.find('integration').full_gem_path}/lib/tasks/*.rake"].each { |ext| load ext }
+Dir["#{Gem.searcher.find('integration').full_gem_path}/lib/tasks/*.rake"].each { |ext| load ext } if defined?(Rake)
