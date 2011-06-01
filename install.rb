41df56df6b6195fa184d0b230d8c067d3996e53e
Sylvestre Mergulhão <contato@mergulhao.info>
2011-06-01 13:15:03 -0300

    remove unnecessary files

--- install.rb
+++
@@ -1,5 +0,0 @@
-require 'fileutils'
-sample = File.expand_path(File.dirname(__FILE__) + "/samples/all_tasks.rake")
-integration = File.expand_path(RAILS_ROOT + "/lib/tasks/integration.rake")
-
-FileUtils.cp sample, integration
