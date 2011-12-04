4199339bda70812ea4fc101033d2158878b3c96a
Sylvestre Mergulhão <contato@mergulhao.info>
2011-12-04 01:44:07 -0200

    remove unecessary stuff from coverage test

--- test/coverage_test.rb
+++ test/coverage_test.rb
@@ -1,12 +1,6 @@
-$: << File.dirname(__FILE__) + '/..' << File.dirname(__FILE__) + '/../lib'
-%w(test/unit rubygems hpricot open-uri).each { |lib| require lib }
+%w(test/unit rubygems hpricot).each { |lib| require lib }
 
 class CoverageTest < Test::Unit::TestCase
-  
-  def deny(condition, message='')
-    assert !condition, message
-  end
-  
   COVERAGE_FILE = "coverage/index.html"
   def test_if_application_is_fully_covered
     doc = Hpricot(File.read(COVERAGE_FILE))
@@ -25,5 +19,4 @@ class CoverageTest < Test::Unit::TestCase
     assert files_without_coverage.empty?, "Bad Boy! Coverage is not 100%... \n Files with problem:\n\t#{files_without_coverage.collect{|file_name| file_name.inner_text}.join("\n\t")}"
     puts "Congratulation! Your coverage is 100%!"
   end
-  
 end
